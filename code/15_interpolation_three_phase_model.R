da7e09a2ae926f7a407ff48124a009eb0bac16a7
larscaspersen <larscaspersen@outlook.de>
2021-04-09 17:18:02 +0200

    adjusted paths of the script. also substituted the fine detailed shape file with a less detailed one, so it saves computation time. one intermediate plot appears to be distorted and I dont know why but the final result looks fine, so I dont fix it

--- code/15_interpolation_three_phase_model.R
+++ code/15_interpolation_three_phase_model.R
@@ -12,36 +12,25 @@ require(tmaptools)
 library(ggplot2)
 library(gridExtra)
 
-ggplot()
-
-station_map <- tm_shape(SA_test) +
-  tm_fill()+
-  tm_shape(Porig) + tm_symbols(size=0.2,shape = 21,col = 'black')+
-  tm_shape(SA)+
-  tm_borders(col='black')+
-  tm_graticules(lines =F,y = seq(-55,5,by=5))+
-  tm_compass(position = c(0.64,0.1))+
-  tm_scale_bar(position = c(0.58,0.01),bg.color = 'white')+
-  tm_layout(legend.outside=T,outer.margins = c(0.001,0.001,0,0.001))
-station_map
-tmap_save(station_map, filename = 'chill_maps/lat_groups.jpg',height = height,width=width,units = 'cm')  
-
+#read south america outline
+SA <- readOGR('data/sa_outline/SA_outline.shp')
 
 #read station coordinates with the projected chill (future and historic)
-stations <- read.csv('southamerica_chill/chill_south_america/data/all_chill_projections.csv')
+stations <- read.csv('data/all_chill_projections.csv')
 
 Porig<-SpatialPointsDataFrame(stations[,c("Longitude","Latitude")],
                               proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"),
-                              data=stations[,c(2,5:53)])
+                              data=stations[,c(2,5:43)])
+
 
 #read shape file of south america with original projection
-Worig<-readOGR('southamerica_chill/chill_south_america/sa_shapefile/SouthAmerica.shp')
+#Worig<-readOGR('southamerica_chill/chill_south_america/sa_shapefile/SouthAmerica.shp')
 
 # Replace point boundary extent with that of South America to make sure the interpolation is done for the whole extend of south america
-Porig@bbox <- Worig@bbox
+Porig@bbox <- SA@bbox
 
 # Create an empty grid where n is the total number of cells
-grd <- as.data.frame(spsample(Worig, "regular", n=50000))
+grd <- as.data.frame(spsample(SA, "regular", n=50000))
 names(grd) <- c("Longitude", "Latitude")
 coordinates(grd) <- c("Longitude", "Latitude")
 gridded(grd) <- TRUE # Create SpatialPixel object
@@ -49,16 +38,17 @@ fullgrid(grd) <- TRUE # Create SpatialGrid object
 proj4string(grd) <- proj4string(Porig)
 
 #save scenario names to vector
-scenarions <- colnames(stations)[6:26]
+scenarions <- colnames(stations)[6:27]
 
 #chose which data is used for the interpolation (this is where later on the loop is set)
-scen <- scenarions[2]
+scen <- scenarions[1]
 
 
 ###### ordinary krigging with mean temp in august 
 
 #read original mean temp in august file
-avg_temp_jul <- raster('southamerica_chill/chill_south_america/data/wc2.1_30s_tavg_07.tif')
+#same file like in script 10
+avg_temp_jul <- raster('D:/chil/wc2.1_30s_tavg/wc2.1_30s_tavg_07.tif')
 
 #set extent to outline of south america
 bb <- extent(-109.46044, -26.23419, -59.48714, 12.62908)
@@ -89,7 +79,7 @@ dat.krg.temp <- krige( f.temp, Porig, grd, dat.fit.temp)
 
 
 r_krig<-raster(dat.krg.temp)
-r.m <- mask(r_krig, Worig)
+r.m <- mask(r_krig, SA)
 
 #calculate a quality map, where you can see the percent difference of krigged temperature to original temperature
 temp_diff <- r.m - temp.res
@@ -97,31 +87,23 @@ temp_diff <- r.m - temp.res
 #temp_qual[temp_qual < -100] <- -NA
 
 #check distribution of quality map
-hist(temp_diff) 
-summary(temp_diff)
+#hist(temp_diff) 
+#summary(temp_diff)
 
 data(World)#for map of south america
 
-library(sf)
-countries <- c('FRA','ARG', 'BOL', 'BRA', 'CHL', 'COL', 'ECU',  'GUY', 'PER', 'PRY', 'SUR', 'URY', 'VEN')
-SA <- World[World$iso_a3 %in% countries,]
-SA_region = extent(c(-8000000,-3000000,-6700000,1700000))
-SA_test <- as_Spatial(SA)
-SA_test <- crop(SA_test,SA_region)
-SA_test <- spTransform(SA_test,CRSobj = crs(Porig))
-
+#size of figures which are saved
 height <- 13
 width <- 12
 
 #plot original temperature
 #jpeg(file='original_temp_august.jpg', width = 700, height = 500)
  
-org_temp <- tm_shape(SA_test) +
+org_temp <- tm_shape(SA) +
   tm_fill()+
   tm_shape(temp.res)+
   tm_raster(n=7, midpoint = 10,palette=get_brewer_pal("-RdBu", n = 9, contrast = c(0, 0.75)),
-            breaks = seq(-15,35,by=5),title="Original Mean Temperature\n in August (°C)",
-            legend.hist=T)+
+            breaks = seq(-15,35,by=5),title="Original Mean Temperature\n in July (°C)")+
   tm_shape(Porig) + tm_symbols(size=0.2,shape = 4,col = 'black')+
   tm_shape(SA)+
   tm_borders(col='black')+
@@ -130,16 +112,15 @@ org_temp <- tm_shape(SA_test) +
   tm_scale_bar(position = c(0.58,0.01),bg.color = 'white')+
   tm_layout(legend.outside=T,outer.margins = c(0.001,0.001,0,0.001))
 org_temp
-tmap_save(org_temp, filename = 'chill_maps/original_tavg_jul.jpg',height = height,width=width,units = 'cm')  
+tmap_save(org_temp, filename = 'figures/interpolation/original_tavg_jul.jpg',height = height,width=width,units = 'cm')  
 
 
 #plot krigged temperature
-krig_temp <- tm_shape(SA_test) +
+krig_temp <- tm_shape(SA) +
   tm_fill()+  
   tm_shape(r.m)+
   tm_raster(midpoint = 10,palette=get_brewer_pal("-RdBu", n = 9, contrast = c(0, 0.75)),
-            breaks = seq(-15,35,by=5),title="Krigged Mean Temperature\nin August (°C)",
-            legend.hist=T)+
+            breaks = seq(-15,35,by=5),title="Krigged Mean Temperature\nin July (°C)")+
   tm_shape(Porig) + tm_symbols(size=0.2,shape = 4,col = 'black')+
   tm_shape(SA)+
   tm_borders(col='black')+
@@ -148,18 +129,18 @@ krig_temp <- tm_shape(SA_test) +
   tm_scale_bar(position = c(0.58,0.01),bg.color = 'white')+
   tm_layout(legend.outside=T,outer.margins = c(0.001,0.001,0,0.001))
 krig_temp
-tmap_save(krig_temp, filename = 'chill_maps/krig_tavg_jul.jpg',height = height,width=width,units = 'cm')  
+tmap_save(krig_temp, filename = 'figures/interpolation/krig_tavg_jul.jpg',height = height,width=width,units = 'cm')  
 
 #tmaptools::palette_explorer()
 
 #plot percent difference of krigged to original temperature
 #jpeg(file='error_krigging_temp_august.jpg', width = 700, height = 500)
-temp_dif <-tm_shape(SA_test) +
+temp_dif <-tm_shape(SA) +
   tm_fill()+
   tm_shape(temp_diff)+
   tm_raster(palette=get_brewer_pal("-RdBu", n = 9, contrast = c(0, 0.75)),
             midpoint = 0,title="Error\nkrig.-orig.(°C)",
-            breaks = c(seq(-10,14,by=4),20),legend.hist=T)+
+            breaks = c(seq(-10,14,by=4),20))+
   tm_shape(Porig) + tm_symbols(size=0.2,shape = 4,col = 'black')+
   tm_shape(SA)+
   tm_borders(col='black')+
@@ -168,31 +149,12 @@ temp_dif <-tm_shape(SA_test) +
   tm_scale_bar(position = c(0.58,0.01),bg.color = 'white')+
   tm_layout(legend.outside=T,outer.margins = c(0.001,0.001,0,0.001))
 temp_dif
-tmap_save(temp_dif, filename = 'chill_maps/difference_tavg_jul.jpg',height = height,width=width,units = 'cm')  
-
-test <- as.data.frame(temp_diff)
-test <- test[!is.na(test$layer),]
-
-ggplot(as.data.frame(temp_diff),aes(x=layer))+geom_histogram(bins = 15)+
-  xlab('Difference krig. - orig. [°C]')+
-  theme_bw()
-ggsave('chill_maps/hist_temp_diff.jpg')
-
-
-round((sum(abs(test) <= 2) / length(test))*100,digits = 1)
+tmap_save(temp_dif, filename = 'figures/interpolation/difference_tavg_jul.jpg',height = height,width=width,units = 'cm')  
 
 
 
-
-
-
-
-
-
-
-
-#plot chill against the mean temperature in august
-ggplot(stations,aes(x = avg_temp_aug, y = X1981)) +
+#plot chill against the mean temperature in july
+ggplot(stations,aes(x = avg_temp_jul, y = X1981)) +
   ylab('Chill Portion')+
   coord_cartesian(ylim = c(0,100))+
   geom_point()+
@@ -209,32 +171,10 @@ mid <- subset(stations, stations$avg_temp_jul > lower_break & stations$avg_temp_
 up <- subset(stations, stations$avg_temp_jul >= upper_break)
 
 #create model for the subset of chill explained by mean temperature in august
-model_low <- lm(data = low, X1981~avg_temp_aug)
-model_mid <- lm(data = mid, X1981~avg_temp_aug)
-model_up <- lm(data = up, X1981~avg_temp_aug)
+model_low <- lm(data = low, X1981~avg_temp_jul)
+model_mid <- lm(data = mid, X1981~avg_temp_jul)
+model_up <- lm(data = up, X1981~avg_temp_jul)
 
-
-ggplot(stations,aes(x = avg_temp_jul, y = X1981)) +
-  ylab('Chill Portion')+
-  #coord_cartesian(ylim = c(0,100))+
-  ylim(c(0,100))+
-  geom_point(data = low, aes(x = avg_temp_jul, y = X1981), col = 'blue')+
-  geom_smooth(data = low, method = 'lm', col = 'blue')+
-  geom_point(data = mid, aes(x = avg_temp_jul, y = X1981), col = 'black')+
-  geom_smooth(data = mid, method = 'lm', col = 'black')+
-  geom_point(data = up, aes(x = avg_temp_jul, y = X1981), col = 'red')+
-  geom_smooth(data = up, method = 'lm', col = 'red')+
-  geom_vline(xintercept=c(lower_break,upper_break))+
-  geom_label_repel(
-    data = subset(stations, outlier),
-    aes(label = Name),
-    box.padding = unit(0.35, "lines"),
-    point.padding = unit(0.3, "lines"),
-    min.segment.length = 0,
-    nudge_x = 5
-  )+
-  theme_bw()
-ggsave('three_phase_model.jpg',  width = 20, height = 15, units = 'cm')
 
 #set up function to automatically calculate the modeled chill by mean temperature in august
 #function is defined for individual cell value, so it needs to be used in loop or in apply function
@@ -290,14 +230,23 @@ raster_model_adjust <- raster(model_adjust)
 raster_model_adjust <- setExtent(raster_model_adjust,bb)
 crs(raster_model_adjust) <- crs(r.m)
 
-jpeg(file='chill_adjustment.jpg', width = 700, height = 500)
 tm_shape(raster_model_adjust)+
+  tm_raster()+
+  tm_shape(SA)+
+  tm_borders()
+
+#something is wrong here. the adjustment layer looks completely distorted
+temp_dif <- tm_shape(SA)+
+  tm_fill()+
+  tm_shape(raster_model_adjust)+
   tm_raster(n=10, palette=get_brewer_pal("RdBu", contrast = c(0, 0.75)),
             midpoint = 0,title="Correction of\n winter chill (Chill Portions)",
             style="cont")+
   tm_shape(Porig) + tm_dots(size=0.2)+
   tm_legend(legend.outside=TRUE)
-dev.off()
+chill_adjust
+#tmap_save(temp_dif, filename = 'figures/interpolation/chill_adjustment.jpg',height = height,width=width,units = 'cm')  
+
 
 
 #do interpolation of chill
@@ -323,38 +272,30 @@ dat.krg.chil <- krige(f.1, Porig, grd, dat.fit)
 
 #assign krigged data to the raster
 r_krig<-raster(dat.krg.chil)
-r.m.chill <- mask(r_krig, Worig)
+r.m.chill <- mask(r_krig, SA)
 r.m.chill<-max(r.m.chill,0)
 
 raster_model_adjust <- setExtent(raster_model_adjust, extent(r.m.chill)) 
 
 #adjust the chill portions, prevent that chill portions become lower than zero
 r<-max(r.m.chill+raster_model_adjust,0)
-r.m <- mask(r, Worig)
+r.m <- mask(r, SA)
 
-jpeg(file='adjusted_chill.jpg', width = 700, height = 500)
-tm_shape(r.m)+
+
+final_chill <- tm_shape(r.m)+
   tm_raster(n=10, palette=get_brewer_pal("RdBu", contrast = c(0, 0.75)),
             midpoint = 0,title="Safe Winter Chill \n(Chill Portions) \nCorrected for Temp.",
             style="cont")+
   tm_shape(Porig) + tm_dots(size=0.2)+
-  tm_legend(legend.outside=TRUE)
-dev.off()
+  tm_graticules(lines =F)+
+  tm_compass(position = c(0.64,0.1))+
+  tm_scale_bar(position = c(0.58,0.01),bg.color = 'white')+
+  tm_layout(legend.outside=T,outer.margins = c(0.001,0.001,0,0.001))
+tmap_save(final_chill, filename = 'figures/interpolation/adjusted_chill_three_phases_1981.jpg',height = height,width=width,units = 'cm')  
 
 
-b <- bbox(annual_mean_temp)
-b[1,] <- c(-85,-30)
-b[2,] <- c(-60,15)
-b <- bbox(t(b))
-
-tm_shape(r.m,bbox = b)+
-  tm_raster(n=10, palette=get_brewer_pal("RdBu", contrast = c(0, 0.75)),
-            midpoint = 50,title="Safe Winter Chill \n(Chill Portions) \nCorrected for Temp.",
-            style="cont")+
-  tm_shape(Porig) + tm_dots(size=0.2)+
-  tm_legend(legend.outside=TRUE)
-
-b <- bbox(avg_temp_aug)
+#have a look at the region with high density of stations
+b <- bbox(avg_temp_jul)
 b[1,] <- c(-75,-65)
 b[2,] <- c(-40,-25)
 b <- bbox(t(b))
@@ -368,12 +309,19 @@ tm_shape(r.m,bbox = b)+
   tm_legend(legend.outside=TRUE)
 
 
-jpeg(file='original_interpolated_chill.jpg', width = 700, height = 500)
-tm_shape(r.m.chill)+
-  tm_raster(n=10, palette=get_brewer_pal("RdBu", contrast = c(0, 0.75)),
-            midpoint = 0,title="Safe Winter Chill \n(Chill Portions)",
-            style="cont")+
+
+orig_krig_chill <- tm_shape(r.m.chill)+
+  tm_raster(palette=get_brewer_pal("RdBu", contrast = c(0, 0.75)),
+            midpoint = 30,title=paste(scen,"\nSafe Winter Chill \n(Chill Portions)\nUncorrected",sep = ''),
+            breaks=seq(0,100,by=10))+
   tm_shape(Porig) + tm_dots(size=0.2)+
-  tm_legend(legend.outside=TRUE)
-dev.off()
+  tm_shape(SA)+
+  tm_borders()+
+  tm_graticules(lines =F)+
+  tm_compass(position = c(0.64,0.1))+
+  tm_scale_bar(position = c(0.58,0.01),bg.color = 'white')+
+  tm_layout(legend.outside=T,outer.margins = c(0.001,0.001,0,0.001))
+orig_krig_chill
+tmap_save(orig_krig_chill, filename = 'figures/interpolation/chill_kriged_uncorrected_1981.jpg',height = height,width=width,units = 'cm')  
+
 
