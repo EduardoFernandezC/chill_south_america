1e612d9d054449b5190bedec982cde0ea1ca7ee4
larscaspersen <larscaspersen@outlook.de>
2021-04-09 15:19:48 +0200

    adjusted paths, is now producing the figures also used in report and presentation

--- code/14_create_adjusment_modelsl.R
+++ code/14_create_adjusment_modelsl.R
@@ -4,11 +4,15 @@
 library(ggplot2)
 library(ggrepel)
 
-stations <- read.csv('southamerica_chill/chill_south_america/data/all_chill_projections.csv')
+stations <- read.csv('data/all_chill_projections.csv')
 
 #hand-defined breaks for three phase model
 upper_break = 20
 lower_break = 8
+
+#mark stations which are outlying in worldclim
+is_outlier <- abs(stations$avg_temp_jul - stations$obs_avg_temp_jul) > 2
+stations$outlier <- is_outlier
 
 ####test mean temp july
 
@@ -22,9 +26,7 @@ model_low <- lm(data = low, X1981~avg_temp_jul)
 model_mid <- lm(data = mid, X1981~avg_temp_jul)
 model_up <- lm(data = up, X1981~avg_temp_jul)
 
-is_outlier <- abs(stations$avg_temp_jul - stations$obs_avg_temp_jul) > 2
-stations$outlier <- is_outlier
-
+#plot three phases model with all stations included, outliers marked by name
 ggplot(stations,aes(x = avg_temp_jul, y = X1981)) +
   ylab('Chill Portion')+
   #coord_cartesian(ylim = c(0,100))+
@@ -38,7 +40,7 @@ ggplot(stations,aes(x = avg_temp_jul, y = X1981)) +
   geom_vline(xintercept=c(lower_break,upper_break))+
   geom_label_repel(
     data = subset(stations, outlier),
-    aes(label = Name),
+    aes(label = station_name),
     box.padding = unit(0.35, "lines"),
     point.padding = unit(0.3, "lines"),
     min.segment.length = 0,
@@ -49,9 +51,9 @@ ggplot(stations,aes(x = avg_temp_jul, y = X1981)) +
 #check model without the supposed outliers
 
 #subset without outliers
-low_clean<- low[!(low$outlier),]
-mid_clean<- mid[!(mid$outlier),]
-up_clean<- up[!(up$outlier),]
+low_clean <- low[!(low$outlier),]
+mid_clean <- mid[!(mid$outlier),]
+up_clean <- up[!(up$outlier),]
 
 #create model for the subset of chill explained by mean temperature in august
 model_low_clean <- lm(data = low_clean, X1981~avg_temp_jul)
@@ -62,7 +64,8 @@ stations_clean <- stations[!stations$outlier,]
 
 ggplot(stations,aes(x = avg_temp_jul, y = X1981)) +
   geom_point(col = 'grey')+
-  ylab('Chill Portion')+
+  ylab('Safe Chill Portion [CP]')+
+  xlab('Average Temperature, July [°C]')+
   #coord_cartesian(ylim = c(0,100))+
   ylim(c(0,100))+
   geom_point(data = low_clean, aes(x = avg_temp_jul, y = X1981), col = 'blue')+
@@ -75,13 +78,16 @@ ggplot(stations,aes(x = avg_temp_jul, y = X1981)) +
   geom_smooth(data = up_clean, method = 'lm', col = 'red',se=F)+
   geom_smooth(data = up, method = 'lm', col = 'red',se=F, linetype = "dashed")+
   geom_vline(xintercept=c(lower_break,upper_break))+
-  ggtitle('Average Temp July')+
   theme_bw()
-#ggsave('three_phases_july.jpg',width = 20, height = 15, units = 'cm')
+ggsave('figures/three_phases_july.jpg',width = 20, height = 15, units = 'cm')
 
 
 
 #####test the same thing with august
+
+#mark outlier
+is_outlier <- abs(stations$avg_temp_aug - stations$obs_avg_temp_aug) > 2
+stations$outlier <- is_outlier
 
 #subset of original station data according to breakes
 low <- subset(stations, stations$avg_temp_aug <= lower_break)
@@ -93,8 +99,7 @@ model_low <- lm(data = low, X1981~avg_temp_aug)
 model_mid <- lm(data = mid, X1981~avg_temp_aug)
 model_up <- lm(data = up, X1981~avg_temp_aug)
 
-is_outlier <- abs(stations$avg_temp_aug - stations$obs_avg_temp_aug) > 2
-stations$outlier <- is_outlier
+
 
 ggplot(stations,aes(x = avg_temp_aug, y = X1981)) +
   ylab('Chill Portion')+
@@ -109,7 +114,7 @@ ggplot(stations,aes(x = avg_temp_aug, y = X1981)) +
   geom_vline(xintercept=c(lower_break,upper_break))+
   geom_label_repel(
     data = subset(stations, outlier),
-    aes(label = Name),
+    aes(label = station_name),
     box.padding = unit(0.35, "lines"),
     point.padding = unit(0.3, "lines"),
     min.segment.length = 0,
@@ -133,7 +138,8 @@ stations_clean <- stations[!stations$outlier,]
 
 ggplot(stations,aes(x = avg_temp_aug, y = X1981)) +
   geom_point(col = 'grey')+
-  ylab('Chill Portion')+
+  ylab('Safe Chill Portion [CP]')+
+  xlab('Average Temperature, August [°C]')+
   #coord_cartesian(ylim = c(0,100))+
   ylim(c(0,100))+
   geom_point(data = low_clean, aes(x = avg_temp_aug, y = X1981), col = 'blue')+
@@ -146,8 +152,7 @@ ggplot(stations,aes(x = avg_temp_aug, y = X1981)) +
   geom_smooth(data = up_clean, method = 'lm', col = 'red',se=F)+
   geom_smooth(data = up, method = 'lm', col = 'red',se=F, linetype = "dashed")+
   geom_vline(xintercept=c(lower_break,upper_break))+
-  ggtitle('Average Temp August')+
   theme_bw()
-#ggsave('three_phases_aug.jpg',width = 20, height = 15, units = 'cm')
+ggsave('figures/three_phases_aug.jpg',width = 20, height = 15, units = 'cm')
 
 
