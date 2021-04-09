26ee36ba8c8bc3e7a3063bb234c2955c534dce66
larscaspersen <larscaspersen@outlook.de>
2021-04-09 12:08:30 +0200

    adjusted the paths

--- code/11_calc_mean_temperatures.R
+++ code/11_calc_mean_temperatures.R
@@ -1,14 +1,15 @@
 #calculate mean temperatures based on original temperature observations of the climate stations
 #in second step compare to data of worldclim to identify outliers
 
-library(stringr)
+library(stringr)#needed for functions to extract the stations name from the file names
 
 
 #read hitoric chill calculated on weather generator data
-setwd('southamerica_chill/chill_south_america/data/patched_temps/')
+setwd('data/patched_temps/')
 temp = list.files(pattern="*.csv")
 temp_files = lapply(temp, read.csv)
 
+#just a test to see if it works
 temp1 <- read.csv('WS_1_ARTURO MERINO BENITEZ INTL.csv')
 sub1 <- temp1[temp1$Month == 7,]
 sub1$t_mean <- round((sub1$Tmax + sub1$Tmin) / 2,digits = 2)
@@ -35,42 +36,50 @@ station_names <- sapply(temp, function(x){
 station_names <- unname(station_names)
 
 #calculate mean temperature and sd of mean temp of jun, jul, aug
-t_jun <- sapply(temp_files, calc_tmean,6)
-t_jun_sd <- sapply(temp_files, calc_tmean_sd,6)
 t_jul <- sapply(temp_files, calc_tmean,7)
 t_jul_sd <- sapply(temp_files, calc_tmean_sd,7)
 t_aug <- sapply(temp_files, calc_tmean,8)
 t_aug_sd <- sapply(temp_files, calc_tmean_sd,8)
 
 #combine to data frame
-df_temp <- data.frame(station_names,t_jun,t_jun_sd,t_jul,t_jul_sd, t_aug, t_aug_sd)
+df_temp <- data.frame(station_names,t_jul,t_jul_sd, t_aug, t_aug_sd)
 
-colnames(df_temp) <- c('Name','obs_avg_temp_jun','sd_obs_avg_temp_jun','obs_avg_temp_jul',
+colnames(df_temp) <- c('Name','obs_avg_temp_jul',
                        'sd_obs_avg_temp_jul','obs_avg_temp_aug','sd_obs_avg_temp_aug')
 
-
+#read stations data frame
 stations <- read.csv('../all_chill_projections.csv')
 
-stations <- merge.data.frame(stations, df_temp, by = 'Name')
+#combine df of stations and observed temperatures
+stations <- merge.data.frame(stations, df_temp, by.x = 'station_name' ,by.y = 'Name')
 
+#update the stations dataframe
 write.csv(stations, file = '../all_chill_projections.csv',row.names = F)
 
 library(ggplot2)
 
+#df needed to draw the ribbon
+df <- data.frame(obs_avg_temp_jul = 0:30, avg_temp_jul = 0:30)
+
 ggplot(stations,aes(x= obs_avg_temp_jul, y = avg_temp_jul)) + 
+  geom_ribbon(data = df, aes(ymin = avg_temp_jul -2, ymax = avg_temp_jul +2), fill="grey", alpha=.5)+
   geom_point() + 
   geom_abline(slope = 1,intercept = 0)+
-  ylab('WorldClim')+
-  xlab('Climate Station')+
-  ggtitle('Mean Temperature in July')+
+  coord_cartesian(xlim = c(0,28),ylim=c(0,28))+
+  ylab('WorldClim: Avergae Temperature, July [°C]')+
+  xlab('Station: Avergae Temperature, July [°C]')+
   theme_bw()
+ggsave('../../figures/WorldClim_vs_Station.jpg')
+
+#shouldnt I also include the sd for this analysis. maybe points on the edge of the ribbon actually overlap it 
+#when sd is included
+
+
 
 is_outlier <- abs(stations$avg_temp_jul - stations$obs_avg_temp_jul) > 2
 stations$outlier <- is_outlier
 
-library(ggrepel)
-
-df <- data.frame(obs_avg_temp_jul = 0:30, avg_temp_jul = 0:30)
+library(ggrepel)#needed to avoid overlap of labels
 
 ggplot(stations,aes(x = obs_avg_temp_jul, y = avg_temp_jul)) +
   geom_ribbon(data = df, aes(ymin = avg_temp_jul -2, ymax = avg_temp_jul +2), fill="grey", alpha=.5)+
@@ -78,7 +87,7 @@ ggplot(stations,aes(x = obs_avg_temp_jul, y = avg_temp_jul)) +
   geom_abline(intercept = 0,slope = 1)+
   geom_label_repel(
     data = subset(stations, outlier),
-    aes(label = Name),
+    aes(label = station_name),
     box.padding = unit(0.35, "lines"),
     point.padding = unit(0.3, "lines"),
     min.segment.length = 0,
@@ -88,6 +97,5 @@ ggplot(stations,aes(x = obs_avg_temp_jul, y = avg_temp_jul)) +
   xlab('Climate Station')+
   ggtitle('Mean Temperature in July')+
   theme_bw()
-
-ggsave('deviation_temp_july.jpg',width = 20,height = 14, units = 'cm')
+#identify the outliers in july
 
