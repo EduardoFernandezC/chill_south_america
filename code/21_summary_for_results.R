149b86c32c8ede895a3a915adf6b7f452beec577
EduardoFernandezC <fernandez.edo.c@gmail.com>
2021-06-07 17:25:30 +0200

    keep working on writing the results

--- code/21_summary_for_results.R
+++ code/21_summary_for_results.R
@@ -5,6 +5,7 @@ library(tmap)
 
 load("figures.RData")
 
+#### Section 3.1 in the manuscript
 
 ### Historic observed panel
 
@@ -45,23 +46,6 @@ quantile(raster::extract(chill_list[["observed_SWC"]], central_valley), 0.95, na
 # Find the extreme values
 chill_list[["scen_2017"]] - chill_list[["scen_1981"]]
 
-# Change values for Patagonia, Argentina
-mean(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], patagonia), na.rm = TRUE)
-quantile(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], patagonia), 0.05, na.rm = TRUE)
-quantile(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], patagonia), 0.95, na.rm = TRUE)
-
-
-# Change values for the Central Valley of Chile
-mean(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], central_valley), na.rm = TRUE)
-quantile(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], central_valley), 0.05, na.rm = TRUE)
-quantile(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], central_valley), 0.95, na.rm = TRUE)
-
-
-# Change values for Brazil
-mean(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], brazil), na.rm = TRUE)
-quantile(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], brazil), 0.05, na.rm = TRUE)
-quantile(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], brazil), 0.95, na.rm = TRUE)
-
 
 # Define the quadrant for Colombia
 colombia <- data.frame(x = seq(-77.5, -72.5, 0.05),
@@ -73,59 +57,176 @@ quantile(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], 
 quantile(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], colombia), 0.95, na.rm = TRUE)
 
 
+# Change values for the Central Valley of Chile
+mean(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], central_valley), na.rm = TRUE)
+quantile(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], central_valley), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], central_valley), 0.95, na.rm = TRUE)
 
 
-b <- bbox(Porig)
-b[1, ] <- c(-77, -70)
-b[2, ] <- c(-37, -30)
-b <- bbox(t(b))
-
-chile <- tm_shape(SA) +
-  tm_fill(col = 'grey10') +
-  tm_shape(SA_test) +
-  tm_lines(col = 'grey35') +
-  tm_shape(r.m) +
-  tm_raster(palette = get_brewer_pal('RdYlBu', n = 20),
-            midpoint = 30,
-            title = 'Safe Winter Chill',
-            style = 'cont', legend.reverse = TRUE, breaks = seq(0, 100, by = 20),
-            legend.format = list(suffix = " CP", text.align = "center")) +
-  tm_shape(Porig) +
-  tm_symbols(size = 0.075, shape = 4, col = 'firebrick', alpha = 0.8) + 
-  tm_shape(SA) +
-  tm_borders(col = 'grey40') +
-  tm_graticules(lines = F, labels.size = 0.6, labels.col = "black") +
-  tm_compass(position = c(0.66, 0.85), text.size = 0.6) +
-  tm_scale_bar(position = c(0.57, 0.925), bg.color = 'transparent', text.size = 0.6, color.dark = "grey20") +
-  tm_add_legend(type = "line", labels = "Excluded", col = "grey35", lwd = 3) +
-  tm_add_legend(type = "symbol", labels = "  Weather station", shape = 4, size = 0.5, col = "firebrick") +
-  tm_layout(main.title = paste0("      ", scenarios_fixed[[scen]]),
-            main.title.position = "center",
-            main.title.size = 1.4,
-            main.title.color = "black",
-            legend.outside = F,
-            legend.title.size = 0.85,
-            legend.text.size = 0.65,
-            legend.position = c(0.665, 0.005),
-            outer.margins = c(0.001, 0.001, 0.001, 0.001),
-            bg.color = "black",
-            attr.color = "white",
-            outer.bg.color = "white")
-
-chile
+# Change values for Patagonia, Argentina
+mean(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], patagonia), na.rm = TRUE)
+quantile(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], patagonia), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], patagonia), 0.95, na.rm = TRUE)
 
 
-figure_3 <- tmap_arrange(chile, chile, chile, chile, ncol = 2)
-
-figure_3
-
-# Save figure 3 with tmap save
-tmap_save(figure_3, "figures/final_figures/test_color.png", width = 17.6, height = 23.4, units = "cm", dpi = 600)
+# Change values for Brazil
+mean(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], brazil), na.rm = TRUE)
+quantile(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], brazil), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["scen_2017"]] - chill_list[["scen_1981"]], brazil), 0.95, na.rm = TRUE)
 
 
+#### Section 3.2 in the manuscript
+
+# Extract the maximum chill values in a continental scale...
+chill_list[["rcp45_2050_pessimistic"]]
+chill_list[["rcp45_2050_optimistic"]]
+
+chill_list[["rcp85_2085_pessimistic"]]
+chill_list[["rcp85_2085_optimistic"]]
 
 
-plot_list[["observed_SWC"]]
+# Focus on specific locations like southern Brazil, northern Patagonia, and the Central Valley of Chile
+
+# Southern Brazil
+# RCP4.5 - 2050
+median(raster::extract(chill_list[["rcp45_2050_pessimistic"]], brazil), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_pessimistic"]], brazil), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_pessimistic"]], brazil), 0.95, na.rm = TRUE)
+
+median(raster::extract(chill_list[["rcp45_2050_optimistic"]], brazil), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_optimistic"]], brazil), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_optimistic"]], brazil), 0.95, na.rm = TRUE)
+
+
+# RCP8.5 - 2085
+median(raster::extract(chill_list[["rcp85_2085_pessimistic"]], brazil), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_pessimistic"]], brazil), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_pessimistic"]], brazil), 0.95, na.rm = TRUE)
+
+median(raster::extract(chill_list[["rcp85_2085_optimistic"]], brazil), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_optimistic"]], brazil), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_optimistic"]], brazil), 0.95, na.rm = TRUE)
+
+
+# Northern Patagonia
+# RCP4.5 - 2050
+median(raster::extract(chill_list[["rcp45_2050_pessimistic"]], patagonia), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_pessimistic"]], patagonia), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_pessimistic"]], patagonia), 0.95, na.rm = TRUE)
+
+median(raster::extract(chill_list[["rcp45_2050_optimistic"]], patagonia), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_optimistic"]], patagonia), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_optimistic"]], patagonia), 0.95, na.rm = TRUE)
+
+
+# RCP8.5 - 2085
+median(raster::extract(chill_list[["rcp85_2085_pessimistic"]], patagonia), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_pessimistic"]], patagonia), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_pessimistic"]], patagonia), 0.95, na.rm = TRUE)
+
+median(raster::extract(chill_list[["rcp85_2085_optimistic"]], patagonia), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_optimistic"]], patagonia), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_optimistic"]], patagonia), 0.95, na.rm = TRUE)
+
+
+
+# Central valley
+# RCP4.5 - 2050
+median(raster::extract(chill_list[["rcp45_2050_pessimistic"]], central_valley), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_pessimistic"]], central_valley), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_pessimistic"]], central_valley), 0.95, na.rm = TRUE)
+
+median(raster::extract(chill_list[["rcp45_2050_optimistic"]], central_valley), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_optimistic"]], central_valley), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_optimistic"]], central_valley), 0.95, na.rm = TRUE)
+
+
+# RCP8.5 - 2085
+median(raster::extract(chill_list[["rcp85_2085_pessimistic"]], central_valley), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_pessimistic"]], central_valley), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_pessimistic"]], central_valley), 0.95, na.rm = TRUE)
+
+median(raster::extract(chill_list[["rcp85_2085_optimistic"]], central_valley), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_optimistic"]], central_valley), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_optimistic"]], central_valley), 0.95, na.rm = TRUE)
+
+
+#### Section 3.3 in the manuscript
+
+# General overview (min and max) from raster elements
+chill_list[["rcp45_2050_pessimistic"]] - median_raster_scen
+chill_list[["rcp45_2050_optimistic"]] - median_raster_scen
+
+
+chill_list[["rcp85_2085_pessimistic"]] - median_raster_scen
+chill_list[["rcp85_2085_optimistic"]] - median_raster_scen
+
+
+# Northern Patagonia, Argentina
+# RCP4.5 - 2050
+median(raster::extract(chill_list[["rcp45_2050_pessimistic"]] - median_raster_scen, patagonia), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_pessimistic"]] - median_raster_scen, patagonia), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_pessimistic"]] - median_raster_scen, patagonia), 0.95, na.rm = TRUE)
+
+median(raster::extract(chill_list[["rcp45_2050_optimistic"]] - median_raster_scen, patagonia), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_optimistic"]] - median_raster_scen, patagonia), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_optimistic"]] - median_raster_scen, patagonia), 0.95, na.rm = TRUE)
+
+
+# RCP8.5 - 2085
+median(raster::extract(chill_list[["rcp85_2085_pessimistic"]] - median_raster_scen, patagonia), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_pessimistic"]] - median_raster_scen, patagonia), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_pessimistic"]] - median_raster_scen, patagonia), 0.95, na.rm = TRUE)
+
+median(raster::extract(chill_list[["rcp85_2085_optimistic"]] - median_raster_scen, patagonia), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_optimistic"]] - median_raster_scen, patagonia), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_optimistic"]] - median_raster_scen, patagonia), 0.95, na.rm = TRUE)
+
+
+# Central valley
+# RCP4.5 - 2050
+median(raster::extract(chill_list[["rcp45_2050_pessimistic"]] - median_raster_scen, central_valley), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_pessimistic"]] - median_raster_scen, central_valley), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_pessimistic"]] - median_raster_scen, central_valley), 0.95, na.rm = TRUE)
+
+median(raster::extract(chill_list[["rcp45_2050_optimistic"]] - median_raster_scen, central_valley), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_optimistic"]] - median_raster_scen, central_valley), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_optimistic"]] - median_raster_scen, central_valley), 0.95, na.rm = TRUE)
+
+
+# RCP8.5 - 2085
+median(raster::extract(chill_list[["rcp85_2085_pessimistic"]] - median_raster_scen, central_valley), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_pessimistic"]] - median_raster_scen, central_valley), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_pessimistic"]] - median_raster_scen, central_valley), 0.95, na.rm = TRUE)
+
+median(raster::extract(chill_list[["rcp85_2085_optimistic"]] - median_raster_scen, central_valley), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_optimistic"]] - median_raster_scen, central_valley), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_optimistic"]] - median_raster_scen, central_valley), 0.95, na.rm = TRUE)
+
+
+# Southern Brazil
+# RCP4.5 - 2050
+median(raster::extract(chill_list[["rcp45_2050_pessimistic"]] - median_raster_scen, brazil), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_pessimistic"]] - median_raster_scen, brazil), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_pessimistic"]] - median_raster_scen, brazil), 0.95, na.rm = TRUE)
+
+median(raster::extract(chill_list[["rcp45_2050_optimistic"]] - median_raster_scen, brazil), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_optimistic"]] - median_raster_scen, brazil), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp45_2050_optimistic"]] - median_raster_scen, brazil), 0.95, na.rm = TRUE)
+
+
+# RCP8.5 - 2085
+median(raster::extract(chill_list[["rcp85_2085_pessimistic"]] - median_raster_scen, brazil), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_pessimistic"]] - median_raster_scen, brazil), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_pessimistic"]] - median_raster_scen, brazil), 0.95, na.rm = TRUE)
+
+median(raster::extract(chill_list[["rcp85_2085_optimistic"]] - median_raster_scen, brazil), na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_optimistic"]] - median_raster_scen, brazil), 0.05, na.rm = TRUE)
+quantile(raster::extract(chill_list[["rcp85_2085_optimistic"]] - median_raster_scen, brazil), 0.95, na.rm = TRUE)
+
+
+
+
 
 
 
